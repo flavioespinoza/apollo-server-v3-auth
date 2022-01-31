8b916d0923ddcace4c24c589250381902f09c62d
Flavio Espinoza <flavio.espinoza@gmail.com>
2022-01-31 08:59:17 -0700

    Directory structure added.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 ﻿# apollo-server-v3-auth
 
 GraphQL authentication with Apollo Server V3 and MongoDB Atlas
+
+## Directory Structure
+
+```shell
+
+├── README.md
+├── graphql
+│   ├── resolvers
+│   │   ├── index.js
+│   │   └── messages.js
+│   └── typeDefs.js
+├── index.js
+├── models # mongoose models
+│   └── Message.js
+├── package.json
+└── yarn.lock
+```
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
 
-const { ApolloServer }  = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const MONGODB = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@apolloserverv3auth.eiiti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const MONGODB = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@apolloserverv3auth.eiiti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
-    .then(() => {
-        console.log("MongoDB Connected");
-        return server.listen({port: 5000});
-    })
-    .then((res) => {
-        console.log(`Server running at ${res.url}`)
-    });
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
